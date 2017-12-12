e5198a640d2231ad11c1e0de87eed99196b35903
Juho Härme <juho.harme@gmail.com>
2017-12-12 17:51:34 +0200

    Getting the texts and converting now works

--- R/collect_examples.R
+++ R/collect_examples.R
@@ -1,22 +1,36 @@
-library(readr)
-adjectives  <- c("Любимый","Милый","Дорогой","Родной")
-adjectives_trans  <- c("lubimyj","milyj","dorogoj","rodnoj")
-examples <- data.frame("adjective"=c(),"pattern"=c(),"context"=c(),stringsAsFactors=F)
-for(adj in adjectives){
-    path <- paste0("Beispiele_txt/",tolower(adj))
-    files <- list.files(path)
-    show(adj)
-    idx <- 0
-    for(this_file in files){
-        show(paste0(idx,"/",length(files)))
-        this_text  <- read_file(paste0("Beispiele_txt/", tolower(adj),"/", this_file))
-        contexts <- as.vector(strsplit(this_text,"\n{2,}")[[1]])
-        pattern  <- gsub(".txt","",this_file,)
-        #pattern  <- gsub(paste0("\\s*(",paste0(adjectives,collapse="|"),paste0(adjectives_trans,collapse="|"),")\\s*"), "",this_file,ignore.case=T)
-        for (context in contexts){
-            examples <- rbind(examples,data.frame("adjective"=adj,"pattern"=pattern,"context"=context))
-        }
-        idx <- idx +1
-    }
+#' @importFrom PythonInR pyExecfile, pyCall
+#' @export
+ConvertExamplesToTextFiles <- function(){
+    fname <- "/home/juho/projects/stockholm/inst/python/collector.py"
+    examples_path <- "/home/juho/juho.harme@gmail.com/events/stockholm17"
+    pyExecfile(fname)
+    ex.list <- pyCall("ConvertToList", examples_path)
 }
-saveRDS(examples,"examples.rds")
+
+#' Collect all examples from the text files
+#'
+#' @return a data frame consisting of all the examples in the specified folder
+#' @importFrom PythonInR pyExecfile, pyCall
+#' @export
+
+GetExamplesFromTextFiles <- function(){
+    #fname <- system.file("python", "collector.py", package="stockholm")
+    fname <- "/home/juho/projects/stockholm/inst/python/collector.py"
+    examples_path <- "/home/juho/juho.harme@gmail.com/events/stockholm17"
+    pyExecfile(fname)
+    ex.list <- pyCall("ConvertToList", examples_path)
+    examples <- data.frame()
+    cat("Converting the list of examples to data frame...","\n")
+    for(idx in 1:length(ex.list)){
+        cat(idx,"/",length(ex.list),"\n")
+        thisrow <- ex.list[[idx]]
+        r <- as.vector(thisrow)
+        names(r) <- names(thisrow)
+        r <- t(as.data.frame(r))
+        examples <- rbind(examples,r)
+    }
+    cat("Done!","\n")
+    return(examples)
+}
+
+
--- inst/python/collector.py
+++ inst/python/collector.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import pypandoc
 import glob
 import os
 import re
 
-def ConvertToTxt():
-    #Convert all word files to plain text
-    for papka in glob.glob("Beispiele/*"):
+def ConvertToTxt(path):
+    """
+    Convert all word files to plain text
+    path: the location of the folder with the examples
+    """
+    for papka in glob.glob(path + "/Beispiele/*"):
         adj = papka[papka.rfind("/")+1:].lower()
         folder_name = "Beispiele_txt/" + adj
         if not os.path.exists(folder_name):
@@ -20,20 +25,27 @@ def ConvertToTxt():
                 print("Error: " + fname)
 
 
-def ConvertToList():
-    """Convert the text files to lists"""
+def ConvertToList(path):
+    """
+    Convert the text files to lists
+    path: the location of the folder with the examples
+    """
     examples = list()
-    for papka in glob.glob("Beispiele_txt/*"):
+    for papka in glob.glob(path + "/Beispiele_txt/*"):
         adj = papka[papka.rfind("/")+1:].lower()
         for txt_file in glob.glob(papka + "/*"):
             fname = txt_file[txt_file.rfind("/")+1:].replace(".txt","")
             pat = re.compile(r"\s*(lubim|mil|dorog|rodn|любим|мил|дорог|родн)(ой|ый|ая|ые|oj|aja|yje|yj)\s*",re.IGNORECASE)
-            pattern = pat.sub(" [adj] ",fname).strip()
+            pattern = pat.sub(" [adj] ",fname).strip().replace(" vok","")
             with open(txt_file,"r") as f:
                 file_content = f.read()
             contexts = re.split(r"\n{2,}", file_content)
             for idx, context in enumerate(contexts):
-                print("{}: {}/{}".format(adj, idx+1, len(contexts)))
-                examples.append({"context":context,"adj":adj,"pattern":pattern})
+                try:
+                    year = re.search(r"\((\d+)[\d-]*\)\s*\]",context).group(1)
+                except AttributeError:
+                    year = "?"
+                examples.append({"context":context,"adj":adj,"pattern":pattern,"year":year})
+            break
+    return examples
 
-ConvertToList()
