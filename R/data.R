763d1e99e0ce2a5bcf96e3a1461aaed37ef09fd9
Juho Härme <juho.harme@gmail.com>
2018-03-05 09:33:59 +0200

    document data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Data extracted from the word files (dorogoj.docx, milyj.docx etc.)
+#'
+#' This data frame includes all the contexts with the adjectives and information about the contexts, e.g. the participiants of the speech situation etc.
+#'
+#' @format A data frame with the following structure:
+#' \describe{
+#'   \item{adj}{the actual adjective in question: milyj, dorogoj, ljubimyj or rodnoj}
+#'   \item{participants}{participants of the speech situation, e.g. brat i sestra}
+#'   \item{year}{The publication year of the original context, NOTE: this is a bit inaccurate}
+#'   \item{tonaltonst}{currently just "other"}
+#'   ...
+#' }
+"bolshaja"
